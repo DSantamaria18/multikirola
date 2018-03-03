d0ab5eb6452e33a92a335d922bc416d08aa705f5
dsantamaria18 <dsantamaria_lago@hotmail.com>
2018-03-03 01:12:37 +0100

    añadido exclusión para páginas estáticas en interceptor

--- grails-app/controllers/multikirola/UserInterceptor.groovy
+++ grails-app/controllers/multikirola/UserInterceptor.groovy
@@ -5,14 +5,21 @@ class UserInterceptor {
     def springSecurityService
 
     UserInterceptor(){
-        matchAll().excludes(uri:'/')
+        matchAll().excludes(uri:'/multikirola/')
+                .excludes(controller: 'home')
                 .excludes(controller: 'login')
                 .excludes(controller: 'user', action: 'register')
                 .excludes(controller: 'user', action: 'save')
+                .excludes(controller: 'actividadMultikirola')
+                .excludes(controller: 'static')
+                .excludes(uri: '/multikirola/error')
+                .excludes(uri: '/multikirola/notFound')
+//                .excludes(uri: '/multikirola/presentacion')
     }
 
     boolean before() {
-        if (!springSecurityService.isLoggedIn() && controllerName != null) {
+        //        if (!springSecurityService.isLoggedIn() && controllerName != null) {
+        if (!springSecurityService.isLoggedIn() && !isExcludedURI(request.requestURI)) {
             redirect(controller: "login", action: "auth")
             return false
         } else {
@@ -24,5 +31,13 @@ class UserInterceptor {
 
     void afterView() {
         // no-op
+    }
+
+    boolean isExcludedURI(final String requestedURI) {
+        def excludedURIs = ['/multikirola/', '/multikirola/error', '/multikirola/notFound', '/multikirola/static/presentacion']
+        boolean result = false
+
+        if (requestedURI in excludedURIs) result = true
+        return result
     }
 }
