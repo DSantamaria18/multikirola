c55db73157af02dc1755ca121ea0116d7b585ec1
dsantamaria18 <dsantamaria_lago@hotmail.com>
2018-03-16 00:54:42 +0100

    cambios necesarios para la nueva home

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -5,6 +5,7 @@ grails.plugin.springsecurity.authority.className = 'multikirola.Role'
 grails.plugin.springsecurity.logout.postOnly = false
 grails.plugin.springsecurity.controllerAnnotations.staticRules = [
         [pattern: '/', access: ['permitAll']],
+        [pattern: '/Home', access: ['permitAll']],
         [pattern: '/error', access: ['permitAll']],
         [pattern: '/index', access: ['permitAll']],
         [pattern: '/index.gsp', access: ['permitAll']],
