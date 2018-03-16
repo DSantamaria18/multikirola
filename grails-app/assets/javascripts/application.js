aa790ceb272151c2c7606a381feb5e155a96a928
dsantamaria18 <dsantamaria_lago@hotmail.com>
2018-03-16 20:29:01 +0100

    fixed header error

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -74,11 +74,11 @@ function validateForm() {
     console.log("Validando datos de registro");
     var formOk = true;
 
-    const username = $('#username').val();
-    const email = $('#useremail').val();
-    const telefono = $('#userphone').val();
-    const movil = $('#usercellphone').val();
-    const whatsapp = $('#whatsapp').prop('checked');
+    var username = $('#username').val();
+    var email = $('#useremail').val();
+    var telefono = $('#userphone').val();
+    var movil = $('#usercellphone').val();
+    var whatsapp = $('#whatsapp').prop('checked');
 
 
     /* validación de username */
@@ -87,7 +87,7 @@ function validateForm() {
         $('#error-username').attr('hidden', false);
         formOk = false;
     } else {
-        const user_re = /^\w+$/; // Sólo caracteres y números
+        var user_re = /^\w+$/; // Sólo caracteres y números
         if (!user_re.test(username)) {
             $('#error-username').text('Caracteres no válidos en el nombre de usuario.');
             $('#error-username').attr('hidden', false);
@@ -96,7 +96,7 @@ function validateForm() {
     }
 
     /* validación de email */
-    const email_re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var email_re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (email == "" || !email_re.test(email)) {
         $('#error-email').text('Debes introducir un email válido.');
         $('#error-email').attr('hidden', false);
@@ -104,7 +104,7 @@ function validateForm() {
     }
 
     /* validación de telefono */
-    const telefono_re = /^(\+34|0034|34)?[6|7|9][0-9]{8}$/;
+    var telefono_re = /^(\+34|0034|34)?[6|7|9][0-9]{8}$/;
     if (telefono == "" || !telefono_re.test(telefono)) {
         $('#error-userphone').text('Debes introducir un teléfono válido.');
         $('#error-userphone').attr('hidden', false);
@@ -125,7 +125,7 @@ function validateForm() {
     }
 
     if ($('#legal').size() > 0) {
-        const legal = $('#legal').prop('checked');
+        var legal = $('#legal').prop('checked');
         if (legal == false) {
             $('#error-legal').text('Debes aceptar las condiciones.');
             $('#error-legal').attr('hidden', false);
@@ -145,48 +145,11 @@ function validateForm() {
     return formOk;
 }
 
-function apuntarParticipantes() {
-    var eventId = $('#eventId').val();
-    var selectedParticpantsIds = [];
-    var selectedParticipants = [];
-    var listaParticipantes = [];
-    var footer = `[id="footer-${eventId}"]`;
-
-    // $('footer .text-capitalize').each(function(){
-    $($(footer), $('.text-capitalize')).each(function(){
-       var nombreParticipante = $(this).text();
-       console.log(nombreParticipante);
-       listaParticipantes.push(nombreParticipante.toLowerCase());
-    });
-
-    $('#checkbox-participante:checked').each(function(){
-        var participanteId = $(this).val();
-        console.log(participanteId);
-        selectedParticpantsIds.push(participanteId);
-        var nombre = $(this).siblings('span').text();
-        if(listaParticipantes.indexOf(nombre.toLowerCase()) < 0 ) {
-            // selectedParticipants.push(nombre);
-            var html = `<div class="participante-row"><div class="participante-datarow"><i class=\"glyphicon glyphicon-user\"></i><span class=\"text-capitalize\"> ${nombre}</span></div><div class="btn btn-danger push eliminar-push" onclick="borraParticipante(this)"> <i class="glyphicon glyphicon-remove"></i></div></div>`
-            $(footer).append(html);
-        }
-    });
-
-    var params = {
-        eventoId: eventId,
-        selectedParticpantsIds: selectedParticpantsIds
-    }
-
-    // $.post("/multikirola/")
-
-};
-
 function passEvent(elem){
     var eventId = $(elem).data('event');
     $('#eventId').val(eventId);
 };
 
-// $('#borrarParticipanteBtn').click(function () {
-// $('.borrar-participante-btn').click(function () {
 function borrarParticipante(elem) {
     var eventId = $('#eventId').val();
     console.log(eventId);
@@ -216,48 +179,6 @@ $('[name="checkbox-participante"]').click(function(){
    }
 });
 
-/*$('#inscribirBtn').click(function(){
-    var eventId = $('#eventId').val();
-    var participantes = [];
-
-
-
-    // for(var i = 0; i < $('[name="checkbox-participante"]:checked').size(); i++){
-    $('[name="checkbox-participante"]:checked').each(function () {
-        var participante = [];
-        var id, telefono, movil;
-        id = $(this).val();
-        console.log(id);
-        participante.push(id);
-
-        telefono = $(this).parents('.input-group').siblings().children('input').eq(0).val();
-        console.log(telefono);
-        participante.push(telefono);
-
-        movil = $(this).parents('.input-group').siblings().children('input').eq(1).val();
-        console.log(movil);
-        participante.push(movil);
-
-        participantes.push(participante);
-        // infoParticipantes.participantes
-    });
-    console.table(participantes);
-    console.log(eventId);
-
-    var infoParticipantes = {
-        eventId: eventId,
-        listaParticipantes: JSON.stringify(participantes)
-    };
-
-    $.post('/multikirola/actividadMultikirola/save', infoParticipantes,
-        function (data, status) {
-            console.log(status);
-        }
-
-    );
-});*/
-
-// $('#inscribirParticipanteBtn').click(function () {
 function inscribirParticipante() {
     var eventId = $('#eventId').val();
     var participanteId = $('#selected-participant-id').val();
@@ -289,21 +210,6 @@ function inscribirParticipante() {
 
     );
 };
-
-/*
-$('#select-participantes').change(function () {
-    var telefono = $('option:selected').attr('phone');
-    var movil = $('option:selected').attr('mobile');
-    var id = $('option:selected').attr('id');
-
-    $('#selected-participant-id').val(id);
-    $('#participante-telefono').val(telefono);
-    $('#participante-mobile').val(movil);
-
-    $('.form-datos-contacto').show();
-    $('.div-inscribirParticipanteBtn').show();
-
-});*/
 
 function showContactInfo() {
     var telefono = $('option:selected').attr('phone');
