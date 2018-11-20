f101d2d0bcb2ed8d00006dddf1b591cf0d52491a
David Santamaria <dsantamaria_lago@hotmail.com>
2018-11-20 18:11:44 +0100

    test para el taglb

---
+++ src/test/groovy/multikirola/UserDetailsTagLibSpec.groovy
@@ -0,0 +1,18 @@
+package multikirola
+
+import grails.testing.web.taglib.TagLibUnitTest
+import spock.lang.Specification
+
+class UserDetailsTagLibSpec extends Specification implements TagLibUnitTest<UserDetailsTagLib> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
