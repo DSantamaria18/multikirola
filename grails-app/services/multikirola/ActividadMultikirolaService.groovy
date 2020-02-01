acb88252c04068b04d556cd1c670fcbcf6f97d6b
David Santamaria <dsantamaria_lago@hotmail.com>
2020-02-01 17:13:20 +0100

    multikirola-136 Añadida imagen

--- grails-app/services/multikirola/ActividadMultikirolaService.groovy
+++ grails-app/services/multikirola/ActividadMultikirolaService.groovy
@@ -46,7 +46,8 @@ class ActividadMultikirolaService {
                 "LEFT JOIN recinto r ON e.recinto_id = r.id " +
                 "LEFT JOIN instalacion i ON e.instalacion_id = i.id " +
                 "LEFT JOIN modalidad m ON e.modalidad_id = m.id " +
-                "WHERE multikirola = TRUE and fecha >= now() " +
+                "WHERE multikirola = TRUE " +
+                "and fecha >= CURDATE() " +
                 "AND modalidad_id IN (1,2,3,5,6,7,8,13,15,19,20,22,24,26,28,32,34,35,37,39,44,49,50,51,52,54,62,67,68) " +
                 "ORDER BY fecha ASC "
 
