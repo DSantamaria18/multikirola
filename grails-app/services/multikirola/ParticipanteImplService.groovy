08692e9bdf7d4817305794e3c0a780f310daf435
David Santamaria <dsantamaria_lago@hotmail.com>
2022-04-30 13:24:12 +0200

    multikirola-168 Correige el filtrado de participantes activos

--- grails-app/services/multikirola/ParticipanteImplService.groovy
+++ grails-app/services/multikirola/ParticipanteImplService.groovy
@@ -10,17 +10,17 @@ class ParticipanteImplService {
     def filtrarParticipantes(params) {
 
         String baseQuery = "SELECT p.id, p.apellido1, p.apellido2, p.nombre, p.sexo, p.fecha_nacimiento AS fechaNacimiento, " +
-                "p.movil, p.email, p.token, c.nombre as centro, cu.nombre as curso, u.enabled as activo " +
+                "p.movil, p.email, p.token, c.nombre as centro, u.enabled as activo " +
                 "FROM participante p, centro c, user u WHERE p.centro_id = c.id AND p.usuario_id = u.id "
         String apellido1Query = (params?.apellido1) ? "AND p.apellido1 LIKE '%${params.apellido1}%' " : ""
         String movilQuery = (params?.movil) ? "AND p.movil LIKE '%${params.movil}%' " : ""
         String emailQuery = (params?.email) ? "AND p.email LIKE '%${params.email}%' " : ""
         String centroQuery = (params.centro != '') ? "AND p.centro_id = ${params.centro} " : ""
-        String activosQuery = (params.activos != '') ? "AND u.enabled = ${params.activos} " : ""
+        String activosQuery = (params.activos != "") ? "AND u.enabled = ${params.activos} " : ""
         String fechaQuery = ((params.fdesde != "-") && (params.fhasta != "-")) ? "AND p.fecha_nacimiento BETWEEN '${params.fdesde}-01' " +
                 "AND '${params.fhasta}-01' " : ""
 
-        String query = baseQuery + apellido1Query + movilQuery + emailQuery + centroQuery + cursoQuery + fechaQuery + activosQuery
+        String query = baseQuery + apellido1Query + movilQuery + emailQuery + centroQuery + fechaQuery + activosQuery
 
         final Sql sql = new Sql(dataSource)
         final results = sql.rows(query)
